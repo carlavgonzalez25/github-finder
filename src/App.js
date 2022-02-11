38448747e893f4f3ca49e4dbc1ed107eed6d51c6
Carla <carla.v.gonzalez@gmail.com>
2022-02-11 14:15:30 -0300

    Creates home and not found components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/Users";
 import User from "./components/users/User";
-import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
-
+import NotFound from "./components/pages/NotFound";
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
+import Home from "./components/pages/Home";
 
 const App = () => {
   return (
@@ -21,22 +20,14 @@ const App = () => {
             <div className="container">
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={() => (
-                    <>
-                      <Search />
-                      <Users />
-                    </>
-                  )}
-                />
+                <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route
                   exact
                   path="/user/:login" //lo que esta en : es un parametro. Luego lo obtenemos como this.props.match.params.login
                   component={User}
                 />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
