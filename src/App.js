84c9e9a6b442e27946e40f051092370e98ee38dd
Carla <carla.v.gonzalez@gmail.com>
2022-02-11 13:01:00 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import User from "./components/users/User";
+import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
+import About from "./components/pages/About";
+
+import GithubState from "./context/github/GithubState";
+import AlertState from "./context/alert/AlertState";
+
+const App = () => {
+  return (
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route
+                  exact
+                  path="/"
+                  render={() => (
+                    <>
+                      <Search />
+                      <Users />
+                    </>
+                  )}
+                />
+                <Route exact path="/about" component={About} />
+                <Route
+                  exact
+                  path="/user/:login" //lo que esta en : es un parametro. Luego lo obtenemos como this.props.match.params.login
+                  component={User}
+                />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
+  );
+};
+
+export default App;
---
+++ src/components/repos/Repos.js
@@ -0,0 +1,14 @@
+import React, { useContext } from "react";
+import GithubContext from "../../context/github/githubContext";
+import RepoItem from "./RepoItem";
+
+const Repos = () => {
+  const githubContext = useContext(GithubContext);
+
+  return (
+    !!githubContext.repos &&
+    githubContext.repos.map((repo) => <RepoItem repo={repo} key={repo.id} />)
+  );
+};
+
+export default Repos;
